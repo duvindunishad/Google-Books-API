6ca244ca2a64caaa7b24dc3b0e64cbdf3d506e41
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2025-01-01 17:03:23 +0530

    create routings

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import BookList from './components/BookList/BookList';
+import BookDetails from './components/BookDetails/BookDetails';
+import Book from './components/BookList/Book';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
+      <Route path='/' element = {<Home />} ></Route>
+      <Route path='about' element = {<About />} ></Route>
+      <Route path='book' element = {<Book />} ></Route>
+      <Route path='/book/:id' element = {<BookDetails />} ></Route>
+      <Route path='booklist' element = {<BookList />} ></Route>
     </Routes>
  </BrowserRouter>
 );
