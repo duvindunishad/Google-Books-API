7c00490a949bc757e1765839dd796cf8cc022e63
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2025-01-02 02:35:00 +0530

    update with the google book api

--- src/context.js
+++ src/context.js
@@ -1,7 +1,5 @@
-import React, { createContext, useContext, useEffect, useState } from
-"react";
-import { useCallback } from "react";
-
+import React, {useState, useContext, useEffect} from 'react';
+import { useCallback } from 'react';
 const URL = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
 const AppContext = React.createContext();
 
@@ -17,8 +15,33 @@ const AppProvider = ({children}) => {
             const response = await fetch(`${URL}${searchTerm}`);
             const data = await response.json();
             const {docs} = data;
-            console.log(docs);
 
+            if(docs){
+                const newBooks = docs.slice(0, 20).map((bookSingle) => {
+                    const {key, author_name, cover_i, edition_count, first_publish_year, title} = bookSingle;
+
+                    return {
+                        id: key,
+                        author: author_name,
+                        cover_id: cover_i,
+                        edition_count: edition_count,
+                        first_publish_year: first_publish_year,
+                        title: title
+                    }
+                });
+
+                setBooks(newBooks);
+
+                if(newBooks.length > 1){
+                    setResultTitle("Your Search Result");
+                } else {
+                    setResultTitle("No Search Result Found!")
+                }
+            } else {
+                setBooks([]);
+                setResultTitle("No Search Result Found!");
+            }
+            setLoading(false);
         } catch(error){
             console.log(error);
             setLoading(false);
--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
 import { AppProvider } from './context';
-
+import './index.css';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import BookList from './components/BookList/BookList';
-import BookDetails from './components/BookDetails/BookDetails';
-import Book from './components/BookList/Book';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element = {<Home />} ></Route>
-      <Route path='about' element = {<About />} ></Route>
-      <Route path='book' element = {<Book />} ></Route>
-      <Route path='/book/:id' element = {<BookDetails />} ></Route>
-      <Route path='booklist' element = {<BookList />} ></Route>
-    </Routes>
- </BrowserRouter>
- </AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
