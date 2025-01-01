d60265d7c444005a85649bbb011ef080f679989b
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2025-01-01 17:32:06 +0530

    update the index css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <main>
+      <Header/>
+      <Outlet/>
+    </main>
   );
 }
 
